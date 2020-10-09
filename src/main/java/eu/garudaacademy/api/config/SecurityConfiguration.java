28f0432bb6684a7bbd3b6047974e8c6d909e0e98
mankamolnar <markmanomolnar@gmail.com>
2020-10-09 18:23:04 +0200

    feature-authentication - Adding authentication configuration to SecurityConfiguration

--- src/main/java/eu/garudaacademy/api/config/SecurityConfiguration.java
+++ src/main/java/eu/garudaacademy/api/config/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package eu.garudaacademy.api.config;
 import eu.garudaacademy.api.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +21,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authentication/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
