f7f3b26c6f5993f23747da8ca23e24bf6ba91d96
mankamolnar <markmanomolnar@gmail.com>
2020-10-10 00:46:06 +0200

    feature-authentication - Connect jtw to mysql users table

--- src/main/java/eu/garudaacademy/api/services/MysqlUserDetailsService.java
+++ src/main/java/eu/garudaacademy/api/services/MysqlUserDetailsService.java
@@ -1,5 +1,7 @@
 package eu.garudaacademy.api.services;
 
+import eu.garudaacademy.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,12 +9,23 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MysqlUserDetailsService implements UserDetailsService {
 
+     @Autowired
+     private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("foo", "foo", new ArrayList<>());
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        final List<eu.garudaacademy.api.models.entity.User> userEntities = userRepository.findByUsername(username);
+
+        if (userEntities.size() > 0) {
+            eu.garudaacademy.api.models.entity.User userEntity = userEntities.get(0);
+            return new User(userEntity.getUsername(), userEntity.getPassword(), new ArrayList<>());
+        }
+
+        return new User(null, null, new ArrayList<>());
     }
 }
