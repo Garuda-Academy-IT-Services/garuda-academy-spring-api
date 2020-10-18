815bccce85b75a19806231c482a98f08fd9614eb
mankamolnar <markmanomolnar@gmail.com>
2020-10-18 22:03:14 +0200

    feature-cors - Add cors to SecurityConfiguration.java

--- src/main/java/eu/garudaacademy/api/config/SecurityConfiguration.java
+++ src/main/java/eu/garudaacademy/api/config/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         configureAllowedPaths(http);
         configureTokenRequestFilter(http);
     }
